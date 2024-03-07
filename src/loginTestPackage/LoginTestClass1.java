12c087ae5716a87521b53f8fc10233dea3fe4f7e
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-07 08:37:26 +0530

    final done

--- src/loginTestPackage/LoginTestClass1.java
+++ src/loginTestPackage/LoginTestClass1.java
@@ -5,10 +5,7 @@ public class LoginTestClass1
 	public void loginclassMethod()
 	{
 		System.out.println("Enter the valid credentials"); 
-		System.out.println("Username Entered and password also enterd");
-		System.out.println("Modified changes done by user 2");
-		
-		
+		System.out.println("Username Entered and  enterd");
 	}
 
 }
