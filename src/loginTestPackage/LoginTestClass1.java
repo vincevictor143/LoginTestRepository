05643a4dcf927f49badec33e6f102aefaa0c11db
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-07 08:00:18 +0530

    Task2 of first person changed the login logic

--- src/loginTestPackage/LoginTestClass1.java
+++ src/loginTestPackage/LoginTestClass1.java
@@ -5,6 +5,8 @@ public class LoginTestClass1
 	public void loginclassMethod()
 	{
 		System.out.println("Enter the valid credentials"); 
+		System.out.println("Enter the userName");
+		System.out.println("Enter the password");
 		
 	}
 
