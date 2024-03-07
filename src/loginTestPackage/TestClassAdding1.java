497315f303d5338f661e2bfb1ebb7da42df2dc08
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-07 07:57:08 +0530

    Task 1 of adding classes is done

---
+++ src/loginTestPackage/TestClassAdding1.java
@@ -0,0 +1,10 @@
+package loginTestPackage;
+
+public class TestClassAdding1 
+{
+	public void testClassMethodAdd()
+	{
+		System.out.println("Class 1 is being added");
+	}
+
+}
---
+++ src/loginTestPackage/TestClassAdding2.java
@@ -0,0 +1,10 @@
+package loginTestPackage;
+
+public class TestClassAdding2 
+{
+	public void testClassMethodAdd2()
+	{
+		System.out.println("Class 2 is being added");
+	}
+
+}
